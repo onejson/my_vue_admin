4ae9254463a56b4b8d63553f1d7da75b49fb6e6d
汪鑫鑫 <971876229@qq.com>
2020-04-02 20:44:36 +0800

    feature(后台列表页面):第一个组件

--- src/components/EnterPage.vue
+++ src/components/EnterPage.vue
@@ -1,113 +1,36 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-         主要按钮，主我是页
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          文字按钮
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div class="CopClass">
+    <h4>{{ msg }}</h4>
+    <el-button type="primary" @click="login">Show Channels</el-button>
+    <el-table :data="channel_data" border justify="center" style="width: 100%">
+      <el-table-column prop="title_chinese" label="ChannelName"></el-table-column>
+      <el-table-column prop="shor_description_chinese" label="Description"></el-table-column>
+      <el-table-column prop="visible" label="Visible"></el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
 export default {
-  name: 'HelloWorld',
+  name: 'CopName',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcom to More DashBoard!',
+      checked: true,
+      channel_data: []
+    }
+  },
+  methods: {
+    login: function () {
+      var url = '/api/channelinfos/'
+      Vue.axios.get(url).then((response) => {
+        this.channel_data = response.data.results
+        console.log('response:' + response)
+      }).catch((error) => {
+        console.log('error!' + error)
+      })
     }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
