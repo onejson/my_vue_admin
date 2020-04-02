75bb1d7261be40e1dcfc3a31d876ff5fa2e220b5
汪鑫鑫 <971876229@qq.com>
2020-04-02 17:59:52 +0800

    feature(后台列表页面):第一个组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import CopClass from '@/components/EnterPage'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/enter',
+      name: 'First',
+      component: CopClass
     }
   ]
 })
