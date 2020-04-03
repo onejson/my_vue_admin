ab1b3066df71cd7af0a49a90e0d5b89bf9b172a8
汪鑫鑫 <971876229@qq.com>
2020-04-03 11:13:16 +0800

    feature(后台列表页面):页面渲染成功

--- src/components/EnterPage.vue
+++ src/components/EnterPage.vue
@@ -3,9 +3,9 @@
     <h4>{{ msg }}</h4>
     <el-button type="primary" @click="login">Show Channels</el-button>
     <el-table :data="channel_data" border justify="center" style="width: 100%">
-      <el-table-column prop="title_chinese" label="ChannelName"></el-table-column>
-      <el-table-column prop="shor_description_chinese" label="Description"></el-table-column>
-      <el-table-column prop="visible" label="Visible"></el-table-column>
+      <el-table-column prop="id" label="Id"></el-table-column>
+      <el-table-column prop="nt_com_nt_num" label="商户编号"></el-table-column>
+      <el-table-column prop="nt_mem_nt_num" label="商户编码"></el-table-column>
     </el-table>
   </div>
 </template>
@@ -27,11 +27,11 @@ export default {
   },
   methods: {
     login: function () {
-      var url = '/api/user/index/'
+      var url = '/api/user/index2/'
       console.log(url)
       Vue.axios.get(url).then((response) => {
-        this.channel_data = response.data.results
-        console.log('response:' + response)
+        this.channel_data = response.data.data
+        console.log('respons22e22332:' + response.data.data[0])
       }).catch((error) => {
         console.log('error!' + error)
       })
