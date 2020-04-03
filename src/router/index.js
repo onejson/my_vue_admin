ea6655eff3025bcf254fd38ead4d4aeda9ce6e63
汪鑫鑫 <971876229@qq.com>
2020-04-03 15:18:49 +0800

    feature(后台列表页面):容器布局

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import CopClass from '@/components/EnterPage'
+import Login from '@/components/Login'
+import VueAxios from 'vue-axios'
+import axios from 'axios'
+import Company from '@/components/company/index'
+import Layout from '@/components/Layout'
 
 Vue.use(Router)
+
+Vue.use(VueAxios, axios)
 
 export default new Router({
   routes: [
@@ -16,6 +23,21 @@ export default new Router({
       path: '/enter',
       name: 'First',
       component: CopClass
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/company/index',
+      name: 'CompanyList',
+      component: Company
+    },
+    {
+      path: '/layout',
+      name: 'Layout',
+      component: Layout
     }
   ]
 })
