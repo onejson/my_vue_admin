bab725eb3aa8de6a0fdb7e4e5602e66a2fca98bc
汪鑫鑫 <971876229@qq.com>
2020-05-08 17:34:00 +0800

    feature(后台列表页面):列表数据渲染

---
+++ src/components/product_channel_configures/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="CopClass">
+    <h4>{{ msg }}</h4>
+    <el-button type="primary" @click="login">新建产品配置组</el-button>
+    <el-table :data="channel_data" border justify="center" style="width: 100%">
+      <el-table-column prop="id" label="id"></el-table-column>
+      <el-table-column prop="nt_mem_nt_num" label="配置组合名称"></el-table-column>
+      <el-table-column prop="nt_mem_nt_num" label="客服电话"></el-table-column>
+      <el-table-column prop="nt_mem_nt_num" label="客服工作时间"></el-table-column>
+      <el-table-column prop="nt_mem_nt_num" label="公司名称"></el-table-column>
+      <el-table-column prop="nt_mem_nt_num" label="计费模版名称"></el-table-column>
+      <el-table-column prop="nt_mem_nt_num" label="操作"></el-table-column>
+    </el-table>
+    <el-pagination
+      background
+      :page-size="20"
+      :pager-count="3"
+      layout="prev, pager, next"
+      :total="100">
+    </el-pagination>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default {
+  name: 'CopName',
+  data () {
+    return {
+      msg: 'Welcom to More DashBoard!',
+      checked: true,
+      channel_data: []
+    }
+  },
+  mounted () {
+    this.login() // 初始化时载入
+  },
+  methods: {
+    login: function () {
+      let url = 'http://127.0.0.1:8080/admin/product_channel_configures/index/'
+      Vue.axios.get(url).then((response) => {
+        this.channel_data = response.data.data
+        console.log('respons22e22332:' + response.data.data[0])
+      }).catch((error) => {
+        console.log('error!' + error)
+      })
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,10 @@ import Router from 'vue-router'
 import Login from '@/components/Login'
 import VueAxios from 'vue-axios'
 import axios from 'axios'
-import Company from '@/components/company/index'
 import Layout from '@/components/Layout'
 import Add from '@/components/Add'
+// eslint-disable-next-line camelcase
+import product_channel_configures_index from '@/components/product_channel_configures/index'
 
 Vue.use(Router)
 
@@ -30,8 +31,8 @@ export default new Router({
       children: [{
         // 这里不设置值，是作为默认页面
         path: '/',
-        name: 'CompanyList',
-        component: Company
+        name: 'product_channel_configures_index',
+        component: product_channel_configures_index
       }, {
         path: '/add',
         name: 'Add',
